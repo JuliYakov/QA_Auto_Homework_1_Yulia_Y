e1a6df229aaefb4cdb34eec9a23a10dcc7d10257
JuliYakov <145484982+JuliYakov@users.noreply.github.com>
2024-07-14 14:03:32 +0300

    Create Home work9-Employee, Manager, Developer, Designer

--- Homework9/homewor9/src/Main.java
+++ Homework9/homewor9/src/Main.java
@@ -1,5 +1,18 @@
+import com.example.employees.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employees = {
+                new Manager("Olena", 26, 3000),
+                new Developer("Vadim", 25, 5000),
+                new Designer("Oleksandr", 23, 2000)
+        };
+
+        for (Employee emp : employees) {
+            emp.performJob();
+            emp.describeRole();
+            emp.describeRole("Age: " + emp.getAge() + ", Salary: $" + (int) emp.getSalary());
+            System.out.println();
+        }
     }
 }
--- Homework9/homewor9/src/com/example/employees/Designer.java
+++ Homework9/homewor9/src/com/example/employees/Designer.java
@@ -1,4 +1,23 @@
 package com.example.employees;
 
-public class Designer {
+public class Designer extends Employee {
+
+    public Designer(String name, int age, double salary) {
+        super(name, age, salary);
+    }
+
+    @Override
+    public void performJob() {
+        System.out.println(getName() + " is designing the interface.");
+    }
+
+    @Override
+    public void describeRole() {
+        System.out.println(getName() + " is a designer.");
+    }
+
+    @Override
+    public void describeRole(String extraInfo) {
+        System.out.println(getName() + " is a designer. " + extraInfo);
+    }
 }
