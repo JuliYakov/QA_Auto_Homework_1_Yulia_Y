8158457b5712849136a760fefafaf852adf27155
JuliYakov <145484982+JuliYakov@users.noreply.github.com>
2024-07-14 15:10:02 +0300

    create homework__9

---
+++ Homework9/untitled/src/com/example/employees/Employee.java
@@ -0,0 +1,46 @@
+package com.example.employees;
+
+public class Employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void work() {
+        System.out.println(getName() + " is working.");
+    }
+
+    public void takeBreak() {
+        System.out.println(getName() + " is taking a break.");
+    }
+
+    public void performJob() {
+        System.out.println(getName() + " is performing job duties.");
+    }
+
+    public void describeRole() {
+        System.out.println(getName() + " is an employee.");
+    }
+
+    public void describeRole(String extraInfo) {
+        System.out.println(getName() + " is an employee. " + extraInfo);
+    }
+}
